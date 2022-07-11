0a439f2d42d3357e4a9590b2d11a524f06d0f472
CoffeeShelter <ij238950@gmail.com>
2022-07-11 17:52:15 +0900

    temp

--- Ncp/src/main/java/data/vo/Student_VO.java
+++ Ncp/src/main/java/data/vo/Student_VO.java
@@ -7,6 +7,15 @@ public class Student_VO {
 	private String major;
 	private String grade;
 	private String sex;
+	private String Professor_number;
+
+	public String getProfessor_number() {
+		return Professor_number;
+	}
+
+	public void setProfessor_number(String professor_number) {
+		Professor_number = professor_number;
+	}
 
 	public String getUniversity_number() {
 		return university_number;
